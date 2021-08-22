2cfc4b808600a4ee587c6c063e8f0659f7bb9c05
Joshua Allford <joshallfordnanook@gmail.com>
2021-08-22 21:55:23 +0100

    Website Complete for now

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ h2.textContent = "This content added by JavaScript";
 
 document.querySelector('body').appendChild(h2);
 
-function mouseOver(){
-    document.getElementById("aboutHead").style.color = "purple";
+
+document.getElementById("event").addEventListener("mouseover", mouseOver);
+document.getElementById("event").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+  document.getElementById("event").style.color = "red";
 }
 
-function mouseOut(){
-    document.getElementById("aboutHead").style.color = "#0032499c";
+function mouseOut() {
+  document.getElementById("event").style.color = "black";
 }
